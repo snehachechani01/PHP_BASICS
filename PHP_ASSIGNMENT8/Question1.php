8e5d580a9286f509500c9dcc712074d404299c81
snehachechani01 <sneha.chechani@sigmainfo.net>
2023-03-03 12:37:10 +0530

    php project

--- PHP_ASSIGNMENT8/Question1.php
+++ PHP_ASSIGNMENT8/Question1.php
@@ -1,5 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RSS FEED</title>
+</head>
+<style>
+    body{
+        font-family: "Times New Roman", Times, serif;
+    }
+   
+    .container{
+        width :80%;
+        margin-left: 10%;
+       
+        text-align: center;
+    }
+    .item img{
+        width:100%;
+        height: 400px;
+    }
+</style>
+<body>
 
+
+<?php
 class RssFeed {
     private $url;
 
@@ -17,19 +43,40 @@ class RssFeed {
             foreach(libxml_get_errors() as $error) {
                 echo "\t", $error->message;
             }
-        }else if($rss_feed===""){
-                echo "";
-            
         } else {
-            echo "<pre>";
-            print_r($rss_feed);
-            echo "</pre>";
+    
+            echo "<div class='container' >";
+            $title2= $rss_feed->channel-> title;
+                echo "<h1>"."$title2"."</h1>";
+                echo "<hr>";
+            foreach ($rss_feed->channel->item as $item) {
+                $title = (string) $item->title; 
+                $link   = (string) $item->link; 
+                $description =  $item->description;
+                
+         echo "<div class='item'";
+        
+             echo "<a href ='".$link."'><h2>".$title ."</a></h2>";
+                echo "<div = 'description'>";
+                 echo "<p>".$description."</p>";
+                echo  "</div>";
+                
+         echo "</div>";
+         echo "<hr>";
+           
+            
         }
-
+        echo "</div>";
+        
+        
     }
-}
+}}
+
 $url = $_POST['rssurl'];
 $rss = new RssFeed($url);
 $rss->getFeed();
 
 ?>
+
+</body>
+</html>
--- PHP_ASSIGNMENT8/index.php
+++ PHP_ASSIGNMENT8/index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
+<style>
+    body{
+    background-color:aliceblue;
+    }
+    </style>
 <body>
 
-<h1>Reading RSS Feed using PHP</h1>
+<h3>Reading RSS Feed using PHP</h3>
 
 <form action="/PHP_BASICS/PHP_ASSIGNMENT8/Question1.php" method="post">
  
   <input type="text"  name="rssurl" placeholder="enter website feed URL" >
-  <input type="submit" value="submit" style="background: yellow; color: white; font-weight: bold; border-color: white; border: 0px; font-size: large;" >
+  <input type="submit" value="submit" style="background: #596578; color: white; font-weight: bold; border-color: blue; border: 10px; font-size: 15px; ">
+  
+    
+   
 </form>
 
 </body>
