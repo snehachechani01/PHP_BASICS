8f96b89f72c8f6623260e6e2c58598d12c81295c
snehachechani01 <sneha.chechani@sigmainfo.net>
2023-03-07 11:18:03 +0530

    php project

--- PHP_ASSIGNMENT9/Question1.php
+++ PHP_ASSIGNMENT9/Question1.php
@@ -2,24 +2,26 @@
 
 class FileUploader
 {
-    public $phpFile;
-    public $phpFileTmp;
+    private $phpFile;
+    private $phpFileTmp;
 
-    public function FileUploader($phpFile, $phpFileTmp)
+    public function __construct($phpFile, $phpFileTmp)
     {
         $this->phpFile = $phpFile;
         $this->phpFileTmp = $phpFileTmp;
     }
 
     public function displayTable()
-    {
-        echo '<table border="1">';
-        $start_row = 1;
+{
+    $destination = "upload/" . basename($this->phpFile);
+    if (move_uploaded_file($this->phpFile, $destination)) {
+        $this->phpFileTmp = $destination;
+        echo "<h1>"."UPDATED CSV FILE"."</h1>";
+        echo '<table>';
         if (($csv_file = fopen($this->phpFileTmp, "r")) !== FALSE) {
             while (($read_data = fgetcsv($csv_file, 1000, ",")) !== FALSE) {
                 $column_count = count($read_data);
                 echo '<tr>';
-                $start_row++;
                 for ($c = 0; $c < $column_count; $c++) {
                     echo "<td>".$read_data[$c] . "</td>";
                 }
@@ -28,37 +30,69 @@ class FileUploader
             fclose($csv_file);
         }
         echo '</table>';
+    } 
+}
+
+
+    public function updateCsv()
+    {
+        if (($handle = fopen($this->phpFileTmp, "r")) !== FALSE) {
+            $updatedRows = array();
+
+            while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                array_unshift($row, "update your data");
+                $updatedRows[] = $row;
+            }
+            array_pop($updatedRows);
+            fclose($handle);
+            if (($handle = fopen($this->phpFileTmp, "w")) !== FALSE) {
+                array_unshift($updatedRows, "update your data");
+                foreach ($updatedRows as $row) {
+                    fputcsv($handle, $row);
+                }
+                fclose($handle);
+            } else {
+                echo "Error opening CSV file for writing!";
+            }
+        } else {
+            echo "Error opening CSV file for reading!";
+        }
     }
 
-    public function updateCsv($newData)
-    {
-        $fp = fopen($this->phpFileTmp, 'a');
-        fputcsv($fp, $newData);
-        fclose($fp);
+
+public function downloadCsv()
+{
+    $url = $this->phpFileTmp;
+    $file_name = basename($this->phpFile);
+
+    $info = pathinfo($file_name);
+    if ($info['extension'] == 'csv') {
+
+        header("Content-Description: File Transfer");
+        header("Content-Type: text/csv");
+        header("Content-Disposition: attachment; filename=\"". $file_name . "\"");
+
+        readfile($url);
+    } else {
+        echo "Sorry, that's not a CSV file!!!";
     }
+
 }
+}
+
+// }
 
 if (isset($_POST['submit'])) {
     $phpFile = $_FILES['php-file']['name'];
     $phpFileTmp = $_FILES['php-file']['tmp_name'];
 
     $fileUploader = new FileUploader($phpFile, $phpFileTmp);
-    $fileUploader->displayTable();
-
-    $newData = array(55, 24, 81, 'N', 11, 100, 151, 'Moon', 'AddedText');
-    $fileUploader->updateCsv($newData);
+    $fileUploader->updateCsv();
 
     $fileUploader = new FileUploader($phpFile, $phpFileTmp);
     $fileUploader->displayTable();
 
-    // header('Content-Type: text/csv');
-    // header('Content-Disposition: attachment; filename="updated_file.csv"');
-    // readfile('/var/www/html/PHP_BASICS/PHP_ASSIGNMENT9/airtravel.csv');
-    // readfile('/var/www/html/PHP_BASICS/PHP_ASSIGNMENT9/biostats(1).csv');
-    // readfile('/var/www/html/PHP_BASICS/PHP_ASSIGNMENT9/cities.csv');
-    
-    // exit;
+    $fileUploader = new FileUploader($phpFile, $phpFileTmp);
+    $fileUploader->downloadCsv();
 }
-
-?>
-
+?>+
